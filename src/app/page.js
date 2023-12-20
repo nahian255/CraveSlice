cc3dbdede3f04e78e4f178a0e7361ab29d83c35b
nahian255 <nahimn253@gmail.com>
2023-12-20 23:42:43 +0600

    start project

---
+++ src/app/page.js
@@ -0,0 +1,17 @@
+
+export default function Home() {
+  return (
+    <main className="min-h-screen ">
+      {/* header part */}
+      <section>
+        <div>
+          <h1>poisjfksfksfk</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde laudantium magnam numquam quisquam aperiam error sapiente temporibus quos enim ducimus iure consectetur quibusdam beatae vitae iusto, atque repudiandae earum voluptas.</p>
+        </div>
+        <div>
+          {/* <Image src={Image} alt='pizza'></Image> */}
+        </div>
+      </section>
+    </main>
+  )
+}
