94d56de96acf1ebab30ed59a3ba678699092d8d7
nahian255 <nahimn253@gmail.com>
2023-12-21 00:01:24 +0600

    navbar done

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,17 @@
+import Image from "next/image";
+import img from '../../public/pizza.png'
 
 export default function Home() {
   return (
     <main className="min-h-screen ">
       {/* header part */}
-      <section>
-        <div>
-          <h1>poisjfksfksfk</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde laudantium magnam numquam quisquam aperiam error sapiente temporibus quos enim ducimus iure consectetur quibusdam beatae vitae iusto, atque repudiandae earum voluptas.</p>
+      <section className="flex">
+        <div className="bg-slate-400 w-1/2 py-8">
+          <h1 className="text-7xl font-semibold">Everything better with a Pizza</h1>
+          <p className="py-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde laudantium magnam numquam quisquam aperiam error sapiente temporibus quos enim ducimus iure consectetur quibusdam beatae vitae iusto, atque repudiandae earum voluptas.</p>
         </div>
         <div>
-          {/* <Image src={Image} alt='pizza'></Image> */}
+          <Image src={img} height={400} width={500} alt="pizza"></Image>
         </div>
       </section>
     </main>
